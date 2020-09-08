c9e1fa64520553d1154e5b31f66fbc3a34c4fde9
Abhidev vv <abhishoot.vv@gmail.com>
2020-09-08 21:20:32 +0530

    Add files via upload

---
+++ python assignment 1 Abhidev vv.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x =[]
+for i in range(2000, 3201):
+        if (i%7==0)and (i%5!=0):
+            x.append(str(i))
+print(','.join(x))
+        
+
+
+# In[2]:
+
+
+First_name = input("Enter your First Name: ")
+Last_name = input("Enter your Last Name: ")
+print(Last_name, First_name)
+
+
+# In[3]:
+
+
+Diameter = int(input("Enter the diameter: "))
+radius =int(Diameter / 2)
+cube = 3
+radius_cube=radius**cube
+Volume_Sphere = (4*22)%(3*7)*radius_cube
+print ("Volume of Sphere is: ", Volume_Sphere)
+
+
+# In[ ]:
+
+
+
+
